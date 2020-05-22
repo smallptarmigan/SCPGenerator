69499b3dffcb81ebf728eda94dfc14e8f607d8fa
smallptarmigan <32320750+smallptarmigan@users.noreply.github.com>
2020-05-22 13:36:21 +0900

    update src

--- src/downloaddata.py
+++ src/downloaddata.py
@@ -1,10 +1,16 @@
+# coding=utf-8
+
 import argparse
+import os
 
 def main(args):
-    pass    
+    output_dir = args.output_dir
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--output_dir', type=str, default='./data/scp/',
+                        help='directory for saving scp data')
     args = parser.parse_args()
     main(args)
-    
